a246a07bf7b7b4809133df1239ce11e552c262ba
bahamert <bersoy@sabanciuniv.edu>
2022-04-10 14:57:00 +0300

    added elementary relationships, basic frontend for signup/login/product regisration testing, resouces

---
+++ back/DB_init_SQL.py
@@ -0,0 +1,139 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+# ONE SHELL MAKE DB BEFORE INITign IT  ! 
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:123123@localhost/STORE'
+
+db=SQLAlchemy(app)
+
+
+#entities 
+
+#table'dan sonraki siralar: table'in adi, columnlar, column adi degeri ve aldigi deger
+customer_product = db.Table('customer_product',
+  #db.Column('Cid', db.Integer, db.ForeignKey('Customer.cid')),
+  db.Column('email', db.String(100), db.ForeignKey('Customers.email')),
+  #db.Column('name', db.String(100), db.ForeignKey('Customers.name')),
+  db.Column('Pid', db.Integer, db.ForeignKey('Products.Pid'))
+)
+
+class Customers(db.Model):
+  __tablename__='Customers'
+  #Cid=db.Column(db.Integer,primary_key=True)
+  name=db.Column(db.String(100))
+  pass_hash=db.Column(db.String(512))
+  email=db.Column(db.String(100), primary_key=True)
+  homeadress=db.Column(db.String(100))
+  bought_products = db.relationship('Products', secondary = customer_product, backref = 'owned')
+
+  def __init__(self,name,pass_hash,email,homeadress):
+    self.name=name
+    self.pass_hash=pass_hash
+    self.email=email
+    self.homeadress=homeadress
+
+class Products(db.Model):
+  __tablename__='Products'
+  Pid=db.Column(db.Integer,primary_key=True, autoincrement=True )
+  name=db.Column(db.String(100))
+  model=db.Column(db.String(50))
+  description=db.Column(db.String(1000))
+  edition_number=db.Column(db.Integer  )
+  quantity =db.Column(db.Integer)
+  price =db.Column(db.Integer )
+  warranty=db.Column(db.String(1000))
+  distributor_Information =db.Column(db.String(1000))
+  sale=db.Column(db.REAL )
+
+  def __init__(self,name,model,description,edition_number,quantity,price,warranty,distributor_Information,sale):
+    self.name=name
+    self.model=model
+    self.description=description
+    self.edition_number=edition_number
+    self.quantity=quantity
+    self.price=price
+    self.warranty=warranty
+    self.distributor_Information=distributor_Information
+    self.sale=sale
+
+    
+
+class Product_Category(db.Model):
+  __tablename__='Product_Category'
+  Pcid=db.Column(db.Integer,primary_key=True)
+  name=db.Column(db.String(100))
+
+  def __init__(self,name):
+    self.name=name
+
+
+class Product_Manager(db.Model):
+  __tablename__='Product_Manager'
+  Pmid=db.Column(db.Integer,primary_key=True)
+  name=db.Column(db.String(100))
+  pass_hash=db.Column(db.String(512))
+
+  def __init__(self,name, pass_hash):
+    self.name=name
+    self.pass_hash=pass_hash
+
+class Sales_Manager(db.Model):
+  __tablename__='Sales Manager'
+  Sid=db.Column(db.Integer,primary_key=True)
+  name=db.Column(db.String(100))
+  pass_hash=db.Column(db.String(512))
+
+  def __init__(self,name, pass_hash):
+    self.name=name
+    self.pass_hash=pass_hash
+
+class Purchased(db.Model):
+  __tablename__='Purchased'
+  purcid=db.Column(db.Integer,primary_key=True)
+  price=db.Column(db.Integer)
+  sale=db.Column(db.REAL)
+  quantity=db.Column(db.Integer)
+  shipment=db.Column(db.String(100))
+
+  def __init__(self,price, sale, quantity, shipment):
+    self.price = price
+    self.sale = sale
+    self.quantity = quantity
+    self.shipment = shipment
+    
+class Comment(db.Model):
+  __tablename__='Comment'
+  cid=db.Column(db.Integer,primary_key=True)
+  text=db.Column(db.String(250))
+  
+
+  def __init__(self,text):
+    self.text=text
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':  #python interpreter assigns "__main__" to the file you run
+  db.create_all()
+  
+
+
--- back/app.py
+++ back/app.py
@@ -1,47 +1,81 @@
+import imp
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
-#https://youtu.be/XZ_gAWdGzZk
+from DB_init_SQL import * 
+
  
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:123123@localhost/students'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:123123@localhost/STORE'
 
 db=SQLAlchemy(app)
-
-class Student(db.Model):
-  __tablename__='students'
-  id=db.Column(db.Integer,primary_key=True)
-  fname=db.Column(db.String(40))
-  lname=db.Column(db.String(40))
-  pet=db.Column(db.String(40))
-
-  def __init__(self,fname,lname,pet):
-    self.fname=fname
-    self.lname=lname
-    self.pet=pet
 
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-  fname= request.form['fname']
-  lname=request.form['lname']
-  pet=request.form['pets']
+@app.route('/signup')
+def signup():
+  return render_template('signup.html')  
 
-  student=Student(fname,lname,pet)
-  db.session.add(student)
+
+
+@app.route('/signup/submit', methods=['POST'])
+def signupsubmit():
+  
+  name= request.form['name']
+  pass_hash=request.form['pass_hash']
+  email=request.form['email']
+  homeadress=request.form['homeadress']
+
+
+
+  
+
+  custumer__= Customers(name,pass_hash,email,homeadress)
+  db.session.add(custumer__)
   db.session.commit()
 
   #fetch a certain student2
-  studentResult=db.session.query(Student).filter(Student.id==1)
-  for result in studentResult:
-    print(result.fname)
+  custumer__=db.session.query(Customers)
+  
+  print(custumer__)
 
-  return render_template('success.html', data=fname)
+  return render_template('success.html', data= name)
+
+
+@app.route('/Products_reg')
+def Products_reg():
+  return render_template('Products_reg.html')
+            
+@app.route('/Products_reg/submit', methods=['POST'])
+def Prsubmit():
+  
+  name=request.form['name']
+  model=request.form['model']
+  description=request.form['description']
+  edition_number=request.form['edition_number']
+  quantity =request.form['quantity']
+  price =request.form['price']
+  warranty=request.form['warranty']
+  distributor_Information =request.form['distributor_Information']
+  sale=request.form['sale']
+
+
+  
+
+  products__ = Products(name,model,description,edition_number, quantity, price, warranty, distributor_Information, sale)
+  db.session.add(products__)
+  db.session.commit()
+
+  #fetch a certain student2
+  products__=db.session.query(Products)
+  
+  print(products__)
+
+  return render_template('success.html', data= name)  
 
 
 if __name__ == '__main__':  #python interpreter assigns "__main__" to the file you run
