04c4d120458bdf29d633ca43916c5cb8223159cc
cem kaya <cemkaya@sabanciuniv.edu>
2022-06-23 13:58:27 +0300

    fixed bug

--- back/crypto_funcs.py
+++ back/crypto_funcs.py
@@ -1,9 +1,9 @@
 from Crypto.Hash import SHA256
 
-def hash_it(input_string):   
+def hash_it(input_string , bitvector = 0 ):   
     #conver input string to byte object 
     input_bytes = input_string.encode()
-
+    #add bit vector if needed for salt and paper 
     hash = SHA256.new(input_bytes) # hash it
     #conver byte object to string
     output_string = hash.hexdigest()
---
+++ back/make_DB2.py
@@ -0,0 +1,32 @@
+import json
+from sys import hash_info
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import requests as req
+from DB_init_SQL import * 
+import crypto_funcs 
+
+ 
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:123123@localhost/STORE'
+
+db=SQLAlchemy(app)
+
+if __name__ =="__main__":   
+   
+    statement = manages.insert().values(Pmid=1, Pid=17)
+    db.session.execute(statement)
+    db.session.commit() 
+
+    statement = change_price.insert().values(Sid=1, Pid=17)
+    db.session.execute(statement)
+    db.session.commit() 
+
+    statement = manages.insert().values(Pmid=1, Pid=18)
+    db.session.execute(statement)
+    db.session.commit() 
+
+    statement = change_price.insert().values(Sid=1, Pid=18)
+    db.session.execute(statement)
+    db.session.commit() 
